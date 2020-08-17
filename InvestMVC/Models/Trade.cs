1681388634b7ce06f19b1e01739f83504053f7ed
Guilherme <guilhermejp4@gmail.com>
2020-08-17 16:32:45 +0200

    Created Trade model and scaffolding

---
+++ InvestMVC/Models/Trade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestMVC.Models
+{
+    public class Trade
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Phone")]
+        [DataType(DataType.Currency)]
+        ulong price { get; set; }
+    }
+}
