7f445ef0cd86f6d20e96134a06b574095b31d253
Guilherme <guilhermejp4@gmail.com>
2020-08-17 10:28:55 +0200

    Registered database context

--- InvestMVC/Startup.cs
+++ InvestMVC/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using InvestMVC.Data;
 
 namespace InvestMVC
 {
@@ -24,6 +21,8 @@ namespace InvestMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<InvestorMVCContext>(options => options.UseSqlServer(Configuration.GetConnectionString("InvestorMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
