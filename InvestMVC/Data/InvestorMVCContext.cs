285a2e084f23ff3eb7d2e5f9197774b8bae8824e
Guilherme <guilhermejp4@gmail.com>
2020-08-17 10:24:40 +0200

    Created DB context and added User list

---
+++ InvestMVC/Data/InvestorMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using InvestMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InvestMVC.Data
+{
+    public class InvestorMVCContext : DbContext
+    {
+
+        public InvestorMVCContext(DbContextOptions<InvestorMVCContext> options) : base(options)
+        
+        {
+            
+        }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
