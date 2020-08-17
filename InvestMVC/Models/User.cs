d18264c25b288a9aa9b55cd1dd9d1a10314a7513
Guilherme <guilhermejp4@gmail.com>
2020-08-17 10:18:14 +0200

    Added base User model and controller

---
+++ InvestMVC/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestMVC.Models
+{
+    public class User
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+        public String email { get; set; }
+        public int phone { get; set; }
+
+    }
+}
