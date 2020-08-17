15d7ae4edd3cbdef011cd8dcfb1541d281601f5c
Guilherme <guilhermejp4@gmail.com>
2020-08-17 16:44:25 +0200

    Created Historical data Model

---
+++ InvestMVC/Models/HistoricalData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestMVC.Models
+{
+    public class HistoricalData
+    {
+        public int Id { get; set; }
+
+        [Required]
+        ulong price{ get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        DateTime time { get; set; }
+    }
+}
