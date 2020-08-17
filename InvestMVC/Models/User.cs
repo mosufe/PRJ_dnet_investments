bfb726eee74655276fb9de407d3d55a233d9634c
Guilherme <guilhermejp4@gmail.com>
2020-08-17 16:29:09 +0200

    Added required field annotations to users model

--- InvestMVC/Models/User.cs
+++ InvestMVC/Models/User.cs
@@ -9,13 +9,19 @@ namespace InvestMVC.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
         public String name { get; set; }
+
+        [Required]
         public String surname { get; set; }
 
+        [Required]
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
         public String email { get; set; }
 
+        [Required]
         [Display(Name = "Phone")]
         [DataType(DataType.PhoneNumber)]
         public int phone { get; set; }
