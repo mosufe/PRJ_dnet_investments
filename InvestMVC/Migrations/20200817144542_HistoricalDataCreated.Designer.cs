f1fc5ca0f8a6b2bf92fa171f6ad94f25d1c4b7c2
Guilherme <guilhermejp4@gmail.com>
2020-08-17 16:46:22 +0200

    Migrated DB with new entity HistoricalData

--- InvestMVC/Migrations/20200817144542_HistoricalDataCreated.Designer.cs
+++ InvestMVC/Migrations/20200817144542_HistoricalDataCreated.Designer.cs
@@ -3,14 +3,16 @@ using InvestMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InvestMVC.Migrations
 {
     [DbContext(typeof(InvestMVCContext))]
-    partial class InvestMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20200817144542_HistoricalDataCreated")]
+    partial class HistoricalDataCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InvestMVC/Migrations/20200817144542_HistoricalDataCreated.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvestMVC.Migrations
+{
+    public partial class HistoricalDataCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+            {
+            migrationBuilder.CreateTable(
+                name: "HistoricalData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoricalData", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoricalData");
+        }
+    }
+}
